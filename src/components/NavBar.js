2ae0bb0f07c6adcc10e12aa5e82947d8a9f7d5f9
Guillermo Velázquez <gago28@gmail.com>
2022-09-21 18:42:47 -0300

    Primer entrega

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import CartWidget from './CartWidget'
 const NavBar = () => {
   return (
     <>
-        <div className='navbar flex justify-between fixed h-20 px-10 w-full bg-neutral-300 shadow-md'>
+        <div className='navbar flex justify-between fixed h-20 px-10 w-full bg-neutral-500 bg-opacity-90 shadow-md'>
             <img src={require("../assets/img/logo3.png")} alt="" 
             className='h-full p-2'/>
             <div className='navRight'>
